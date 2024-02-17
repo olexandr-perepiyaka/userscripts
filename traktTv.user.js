7f4b80016d3932e344872e17f2a7ca4bfd83de73
olexandr-perepiyaka <38291676+olexandr-perepiyaka@users.noreply.github.com>
2024-02-17 12:18:34 +0200

    Create traktTv.user.js

---
+++ traktTv.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         traktTv.user.js
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  Script for Trakt.Tv pages
+// @author       alex.perepiyaka@gmail
+// @match        https://trakt.tv/*
+// @downloadURL  https://raw.githubusercontent.com/olexandr-perepiyaka/userscripts/master/traktTv.user.js
+// @updateURL    https://raw.githubusercontent.com/olexandr-perepiyaka/userscripts/master/traktTv.user.js
+// ==/UserScript==
